8bca249c7f71e04f9a3c6fa046c359b9f4ea816c
chakravarthy-petluri <99456248+chakravarthy-petluri@users.noreply.github.com>
2025-02-11 19:17:09 -0600

    Initial commit

---
+++ src/main/java/com/petluri/gen_ai_text_classifier/GenAiTextClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.petluri.gen_ai_text_classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiTextClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiTextClassifierApplication.class, args);
+	}
+
+}
